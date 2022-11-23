02e5406e940d34f1204509d6a7d31d46be7fe756
MiguelV <miguel.cardoso-vieira@insa-lyon.fr>
2022-11-23 14:50:27 +0100

    Coding challenge 3

---
+++ CMakeFiles/state_machine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rtos-sim/Desktop/FORK/git-tutorial/src/main.c" "/home/rtos-sim/Desktop/FORK/git-tutorial/CMakeFiles/state_machine.dir/src/main.c.o"
+  "/home/rtos-sim/Desktop/FORK/git-tutorial/src/my_states.c" "/home/rtos-sim/Desktop/FORK/git-tutorial/CMakeFiles/state_machine.dir/src/my_states.c.o"
+  "/home/rtos-sim/Desktop/FORK/git-tutorial/src/new_features.c" "/home/rtos-sim/Desktop/FORK/git-tutorial/CMakeFiles/state_machine.dir/src/new_features.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/rtos-sim/Desktop/FORK/git-tutorial/CMakeFiles/STATES_LIB.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/state_machine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/state_machine.dir/src/main.c.o"
+  "CMakeFiles/state_machine.dir/src/new_features.c.o"
+  "CMakeFiles/state_machine.dir/src/my_states.c.o"
+  "/home/rtos-sim/Desktop/FORK/bin/state_machine.pdb"
+  "/home/rtos-sim/Desktop/FORK/bin/state_machine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/state_machine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
